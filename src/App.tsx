5e3dd42c10e5088493e07f626228bdb2157d00a9
Nélio <jrneliodias@gmail.com>
2025-02-11 23:13:21 -0300

    Add Chuck Norris Jokes app structure with search and footer components

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 import './App.css'
+import { SearchBar } from './components/ui/SearchBar'
+import { Footer } from './components/ui/Footer'
+
 
 function App() {
-
   return (
-    <main>
-      <h1>Hello World</h1>
+    <main className='flex flex-col items-center h-screen gap-10'>
+      <h1 className='text-4xl font-bold hover:text-5xl transition-all duration-300'>Chuck Norris Jokes</h1>
+      <SearchBar />
+
+      <Footer />
     </main>
   )
 }
---
+++ src/types/joke.ts
@@ -0,0 +1,8 @@
+export interface Joke {
+  id: string;
+  value: string;
+  categories: string[];
+  created_at: string;
+  updated_at: string;
+  url: string;
+}
